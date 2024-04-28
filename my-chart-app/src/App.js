154434f438c96efdb26fcd0142e0650a59ea041a
lakshay Garg <108176576+lgarg12@users.noreply.github.com>
2024-04-29 01:22:52 +0530

    Big Data Project

---
+++ my-chart-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ChartComponent from './Components/ChartComponent';
+
+function App() {
+  return (
+    <div className="">
+      <ChartComponent/>
+    </div>
+  );
+}
+
+export default App;
